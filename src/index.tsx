fd8e381c9b05a3732b8e2af5596a260624e27ca7
Evgeniy Shapechko <shapen86@mail.ru>
2023-05-29 20:09:06 +0300

    day 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ToDoList } from "./pages/ToDoList";
+import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>

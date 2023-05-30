54697aabbf99c70a7c0eff97bf1f49101e03e21d
Evgeniy Shapechko <shapen86@mail.ru>
2023-05-30 11:16:49 +0300

    day 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ToDoList } from "./pages/ToDoList";
-import "./index.scss";
+import { ToDoListPage } from "./pages/ToDoListPage";
+import "./assets/scss/normalize.scss";
+import "./assets/scss/style.scss";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <ToDoList />
+        <ToDoListPage />
     </React.StrictMode>
 );

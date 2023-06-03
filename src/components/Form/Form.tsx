e66114f319f3cfd4427fb3cff37997b8352399b1
Evgeniy Shapechko <shapen86@mail.ru>
2023-06-03 11:55:27 +0300

    style form with styled-component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,6 @@
-import "./Form.scss";
 import { useState } from "react";
+import { FormControl, FormBlock, FormField, FormLabel, FormWrapper } from "./Form.styled";
+import plusIcon from "../../assets/images/plus.png";
 
 export const Form = (props: { createNewToDo: Function }) => {
     const [text, setText] = useState<string>("");
@@ -13,13 +14,13 @@ export const Form = (props: { createNewToDo: Function }) => {
     };
 
     return (
-        <div className="form-wrapper">
-            <form action="#" onSubmit={formSubmit}>
-                <label>
-                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-                    <button></button>
-                </label>
-            </form>
-        </div>
+        <FormWrapper>
+            <FormBlock action="#" onSubmit={formSubmit}>
+                <FormLabel>
+                    <FormField type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                    <FormControl icon={plusIcon} />
+                </FormLabel>
+            </FormBlock>
+        </FormWrapper>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/scss/normalize.scss";
-import "./assets/scss/style.scss";
 import { RouterProvider } from "react-router-dom";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
 import { router } from "./router";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
+            <GlobalStyle />
             <RouterProvider router={router} />
         </Provider>
     </React.StrictMode>
